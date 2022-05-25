962c5c8c3dad62f136d93df0859c0e135e3a5e12
MNewbigging <mrnewbigging@gmail.com>
2022-05-25 20:06:58 +0100

    icon sizing

--- src/cards/icons/LocalIcon.tsx
+++ src/cards/icons/LocalIcon.tsx
@@ -1,9 +1,15 @@
+import './local-icon.scss';
+
 import LocalIconLogo from '/assets/local.svg';
 import React from 'react';
 
 export const LocalIcon: React.FC = () => {
   return (
-    <div className='icon-logo' style={{ width: '22px', height: '22px' }} title={'Offline'}>
+    <div
+      className='icon-logo local-icon'
+      style={{ width: '22px', height: '22px' }}
+      title={'Offline'}
+    >
       <LocalIconLogo />
     </div>
   );
--- src/cards/icons/OnePlayerIcon.tsx
+++ src/cards/icons/OnePlayerIcon.tsx
@@ -1,9 +1,15 @@
+import './one-player-icon.scss';
+
 import OnePlayerLogo from '/assets/onePlayer.svg';
 import React from 'react';
 
 export const OnePlayerIcon: React.FC = () => {
   return (
-    <div className='icon-logo' style={{ width: '22px', height: '22px' }} title={'One Player'}>
+    <div
+      className='icon-logo one-player-icon'
+      style={{ width: '22px', height: '22px' }}
+      title={'One Player'}
+    >
       <OnePlayerLogo />
     </div>
   );
