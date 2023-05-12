49c3c006052b93b1f8b7003d534e6ffff0e528e6
MNewbigging <mrnewbigging@gmail.com>
2023-05-12 17:59:38 +0100

    removed wip cards

--- src/screens/AppsScreen.tsx
+++ src/screens/AppsScreen.tsx
@@ -4,9 +4,7 @@ import { BletherCard } from '../cards/apps/blether/BletherCard';
 import { CodeCompendiumCard } from '../cards/apps/code-compendium/CodeCompendiumCard';
 import { GridPlannerCard } from '../cards/apps/grid-planner/GridPlannerCard';
 import { KeyboardistCard } from '../cards/apps/keyboardist/KeyboardistCard';
-import { SoundshapesCard } from '../cards/apps/soundshapes/SoundshapesCard';
 import { TaskLogCard } from '../cards/apps/task-log/TaskLogCard';
-import { TbgPlatformCard } from '../cards/apps/tbg-platform/TbgPlatformCard';
 
 export const AppsScreen: React.FC = () => {
   return (
@@ -16,8 +14,6 @@ export const AppsScreen: React.FC = () => {
       <CodeCompendiumCard />
       <TaskLogCard />
       <KeyboardistCard />
-      <SoundshapesCard />
-      <TbgPlatformCard />
     </>
   );
 };
